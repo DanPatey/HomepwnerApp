af4dcfd8ae7a8b966f0c639d129e93088c63562b
Dan Patey <danpatey@danpatey.com>
2016-06-30 22:30:50 -0700

    inital commit; created Item class to create random UITable information

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Dan Patey on 6/30/16.
+//  Copyright © 2016 Dan Patey. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
