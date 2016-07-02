609aca0164623426e9abebd73de6e502b9a430da
Dan Patey <danpatey@danpatey.com>
2016-07-01 21:33:23 -0700

    Implementing removal functionality

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class ItemStore {
     var allItems = [Item]()
-        
+    
     func createItem() -> Item {
         let newItem = Item(random: true)
         
         allItems.append(newItem)
         
         return newItem
+    }
+    
+    func removeItem(item: Item) {
+        if let index = allItems.indexOf(item) {
+            allItems.removeAtIndex(index)
+        }
     }
 }
