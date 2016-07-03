a2793f8a73c0d94cd2177b80d9766ed40c71ccf2
Dan Patey <danpatey@danpatey.com>
2016-07-03 13:02:03 -0700

    Resolving build info and fun with passing data

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -94,6 +94,19 @@ class ItemsViewController: UITableViewController {
         itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // If the triggered segue is the "ShowItem" segue
+        if segue.identifier == "ShowItem" {
+            // Figure out which row was just tapped
+            if let row = tableView.indexPathForSelectedRow?.row {
+                // Get the item associated with this row and pass it along
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destinationViewController as! DetailViewController
+                detailViewController.item = item
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
